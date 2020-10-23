7845c21dbe9bbf73dd11f29dbf6def7d407a24d4
CCsaurabh <saurabhm2795@gmail.com>
2020-10-23 19:52:01 +0530

    Update DateTime_Modify.py

--- DateTime_Modify.py
+++ DateTime_Modify.py
@@ -1,1 +1,20 @@
+"""
+
+https://machinelearningmastery.com/resample-interpolate-time-series-data-python/
+Example-01
+Suppose the data is not having any year and the data is only in the form of the months the following can help in formatting.
+
+""
+
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+
+def parser(x):
+  return datetime.strptime('190'+x,'%Y-%m')
+  
+series=read_csv('data.csv',header=0,parser_dates=[0],index_col=0, squeeze=True, date_parser=parser)
+print(series.head())
+series.plot()
+pyplot.show()
 
