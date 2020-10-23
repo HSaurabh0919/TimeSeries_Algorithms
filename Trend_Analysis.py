3fbc9ca4eda89c5c5a942d4b007a911ecd682771
CCsaurabh <saurabhm2795@gmail.com>
2020-10-23 19:50:29 +0530

    Update Trend_Analysis.py

--- Trend_Analysis.py
+++ Trend_Analysis.py
@@ -1,13 +1,13 @@
 #Checking for the trend in the data
 
 # We can check through seasonal decomposition, moving average and the linear regression modelling for checking the trend in the data
-fig=plt.figure(figsize=(20,18))
+fig=plt.figure(figsize=(20,20))
 layout=(3,2)
-pm_ax=plt.subplot2grid(layout,(0,0),colspan=2)
+pm_ax=plt.subplot2grid(layout,(0,0),colspan=2) #For seasonal_decomposition
 mv_ax=plt.subplot2grid(layout,(1,0),colspan=2)
 fig_ax=plt.subplot2grdi(layout,(2,0),colspan=2)
 
-# Automatic decomposing through seasonal decomposition
+# Automatic decomposing through seasonal decomposition 
 plt.figure(num=None, figsize=(50, 20), dpi=80, facecolor='w', edgecolor='k')
 series = air_pollution.pollution_today[:365]
 result = seasonal_decompose(series, model='multiplicative')
